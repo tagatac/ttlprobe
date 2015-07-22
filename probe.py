cce2702a905cae546da600acc08244d1ec8fcc15
David Tagatac <dtagatac@cs.columbia.edu>
2015-07-22 18:35:16 -0400

    ValueError: check_hostname requires server_hostname

--- probe.py
+++ probe.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-import socket, time, httplib
+import socket, time, ssl
 
 IP = '220.181.112.244'
 PORT = 80
 BUFFER_SIZE = 1024
-MESSAGE = 'GET /?falun HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+MESSAGE = 'GET /?test HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
+CONTEXT = ssl.create_default_context()
 
 for ttl in range(0, 25):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	s = CONTEXT.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 	s.connect((IP, PORT))
 	s.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 	s.send(MESSAGE)
