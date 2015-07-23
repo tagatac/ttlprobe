ced2a5b15d92b2c3941c34c3ea5c9102bb310b8b
David Tagatac <dtagatac@cs.columbia.edu>
2015-07-23 15:39:43 -0400

    SSL/TLS results (no RST packets from GFW)

--- probe.py
+++ probe.py
@@ -3,14 +3,14 @@
 import socket, time, ssl
 
 IP = '220.181.112.244'
-PORT = 80
+PORT = 443
 BUFFER_SIZE = 1024
-MESSAGE = 'GET /?test HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
-CONTEXT = ssl.create_default_context()
+MESSAGE = 'GET /?falun HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
 
-for ttl in range(0, 25):
-	
-	s = CONTEXT.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+ctx = ssl.create_default_context()
+for ttl in range(25):
+	#s = ctx.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname='www.baidu.com')
+	s = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), ssl_version=ssl.PROTOCOL_SSLv3)
 	s.connect((IP, PORT))
 	s.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
 	s.send(MESSAGE)
