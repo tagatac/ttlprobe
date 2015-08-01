f305fb47a2399bffe5e7f2db2c75a22663820107
David Tagatac <dtagatac@cs.columbia.edu>
2015-07-31 20:44:44 -0400

    basic crawler without selenium

---
+++ crawler/crawler/spiders/topchinese.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from crawler.items import CrawlerItem
+
+from selenium import selenium
+
+class TopChinese(CrawlSpider):
+    name = 'topchinese'
+    allowed_domains = ['baidu.com']
+    start_urls = ['http://www.baidu.com/']
+    #custom_settings = {'DEPTH_LIMIT': 2}
+
+    rules = (
+        Rule(LinkExtractor(allow_domains=allowed_domains), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+	i = CrawlerItem()
+	i['url'] = response.url
+	yield i
