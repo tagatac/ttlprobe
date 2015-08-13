c148f3ed63fcd388f840c0fb5c6beff493bd5523
David Tagatac <dtagatac@cs.columbia.edu>
2015-08-12 20:01:40 -0400

    refactored download.py; close connections after download attempts

--- download.py
+++ download.py
@@ -1,112 +1,58 @@
-#!/usr/bin/env python
-import socket, subprocess, ssl, os, json, sys
+#!/usr/bin/env python3
+import argparse, os, sys, json
+from utils import *
 
-BUFSIZE = 4096
-CTX = ssl.create_default_context()
-STORAGE_DIR = 'receivedfiles'
 REPEAT_COUNT = 1000
+FILE_LIST = 'jsfiles.json'
+STORAGE_DIR = 'downloadfiles'
 
-class URIError(Exception):
-	"""Exception raised for a malformed URI.
+# commandline argument parser
+parser = argparse.ArgumentParser(description='Determine the smallest TTL required to download various JS files from China.')
+parser.add_argument('-r', '--repeat', default=REPEAT_COUNT, type=int, help='the number of times to download each file')
+parser.add_argument('-f', '--filelist', default=FILE_LIST, type=str, help='the path to the file containing the list of candidate files to be downloaded')
+parser.add_argument('-d', '--dir', default=STORAGE_DIR, type=str, help='the path at which to store the downloaded files')
+#parser.add_argument('-o', '--outfile', default=RESULTS_FILE, type=str, help='the path of the file in which to store the results')
+args = parser.parse_args()
 
-	Attributes:
-		numsegs -- the number of slash-delimited segments in the
-			   malformed URI
-	"""
-	def __init__(self, numsegs):
-		self.numsegs = numsegs
+# make sure the download folder and results file don't exist yet
+if os.path.exists(args.dir):
+	print('ERROR: Download directory ' + args.dir  + '  already exists')
+	sys.exit()
+#if os.path.exists(args.outfile):
+#	print('ERROR: Outfile ' + args.outfile  + '  already exists')
+#	sys.exit()
 
-class ProtocolError(Exception):
-	"""Exception raised when an unsupported protocol is used.
-
-	Attributes:
-		prtcl -- the unsupported protocol
-	"""
-	def __init__(self, prtcl):
-		self.prtcl = prtcl
-
-# Parse a JS file URI, and return whether TLS is used, the host, and the path to
-# the JS file.
-def parseURI(URI):
-	splitURI = URI.split('/')
-	if len(splitURI) < 4: raise URIError(len(splitURI))
-	if splitURI[0] == 'https:': tls = True
-	elif splitURI[0] == 'http:': tls = False
-	else: raise ProtocolError(splitURI[0])
-	host = splitURI[2]
-	request = '/' + '/'.join(splitURI[3:])
-	filename = splitURI[-1]
-	return tls, host, request, filename
-
-# Perform a TCP traceroute to the host, and return the number of hops required.
-def traceroute(host):
-	traceroute_output = subprocess.check_output(['sudo', 'tcptraceroute',
-						     host])
-	return int(traceroute_output.split('\n')[-2].split()[0])
-
-# Construct the HTTP GET request string.
-def gen_message(host, request, referer):
-	message = 'GET /' + request + ' HTTP/1.1\r\n'
-	message += 'Host: ' + host + '\r\n'
-	message += 'Connection: close\r\n'
-	message += 'Cache-Control: max-age=0\r\n'
-	message += 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4)'
-	message += ' AppleWebKit/537.36 (KHTML, like Gecko) '
-	message += 'Chrome/44.0.2403.125 Safari/537.36\r\n'
-	message += 'Referer: ' + referer + '\r\n'
-	message += 'Accept-Encoding: identity\r\n'
-	message += 'Accept-Language: en-US,en;q=0.8\r\n\r\n'
-	return message
-
-# Issue the HTTP GET request repeatedly for a given referer and script, saving
-# the file if one is received (rare)
-def issue_requests(tls, host, message, ttl, filename):
-	for i in range(REPEAT_COUNT):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		if tls:
-			s = CTX.wrap_socket(s, server_hostname=host)
-			port = 443
-		else:
-			port = 80
-		s.connect((host, port))
-		s.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
-		s.send(message)
-		full_response = ''
-		while True:
-			try:
-				response = s.recv(BUFSIZE)
-				if response: full_response += response
-				else: break
-			except:
-				break
-		if full_response:
-			path = os.path.join(STORAGE_DIR, host)
-			if not os.path.isdir(path): os.makedirs(path)
-			with open(os.path.join(path, str(i) + '_' + filename),
-				  'w') as f:
-				f.write(full_response.split('\r\n')[-1])
-
-distance_table = dict() #number of hops to each host
+# get the list of JS files
 with open('jsfiles.json') as f: jsondata = json.load(f)
+
+# parse the URIs, generate the request messages, and issue the requests
+distance_table = dict() #number of hops to each host
 for referer in jsondata:
 	for script in referer['scripts']:
 		try:
 			tls, host, request, filename = parseURI(script)
 		except URIError as e:
-			print 'The URI ' + script + ' is malformed. ' + \
-			      'There should be at least 4 slash-delimited ' + \
-			      'segments, but there are only ' + str(e.numsegs)
+			print('The URI ' + script + ' is malformed. ' +
+			      'There should be at least 4 slash-delimited ' +
+			      'segments, but there are only ' + str(e.numsegs))
 			continue
 		except ProtocolError as e:
-			print 'Protocol ' + e.prtcl + \
-				' is not allowed. Skipping file: ' + script
+			print('Protocol ' + e.prtcl +
+			      ' is not allowed. Skipping file: ' + script)
 			continue
 		if host not in distance_table:
 			distance_table[host] = traceroute(host)
 		ttl = distance_table[host] - 1
 		message = gen_message(host, request, referer['referer'])
-		print 'Requesting ' + script + ' ' + str(REPEAT_COUNT) \
-		      + ' times, referred by ' + referer['referer'] + \
-		      ', with TTL=' + str(ttl)
+		print('Requesting ' + script + ' ' + str(args.repeat)
+		      + ' times, referred by ' + referer['referer'] +
+		      ', with TTL=' + str(ttl))
 		sys.stdout.flush()
-		issue_requests(tls, host, message, ttl, filename)
+		for i in range(args.repeat):
+			response, dowload_time = issue_request(host, message, tls, ttl)
+			if response:
+				path = os.path.join(args.dir, host)
+				if not os.path.isdir(path): os.makedirs(path)
+				with open(os.path.join(path, str(i) + '_' + filename),
+					  'wb') as f:
+					f.write(response.split(b'\r\n')[-1])
---
+++ utils.py
@@ -0,0 +1,87 @@
+# utils.py - various utilities useful for the TTL probe
+import ssl, subprocess, socket, time
+
+BUFSIZE = 4096
+SSL_CONTEXT = ssl.create_default_context()
+
+class TTLProbeError(Exception):
+	"""Base class for TTL probe exceptions."""
+	pass
+
+class URIError(TTLProbeError):
+	"""Exception raised for a malformed URI.
+
+	Attributes:
+		numsegs -- the number of slash-delimited segments in the
+			   malformed URI
+	"""
+	def __init__(self, numsegs):
+		self.numsegs = numsegs
+
+class ProtocolError(TTLProbeError):
+	"""Exception raised when an unsupported protocol is used.
+
+	Attributes:
+		prtcl -- the unsupported protocol
+	"""
+	def __init__(self, prtcl):
+		self.prtcl = prtcl
+
+# Parse a JS file URI, and return whether TLS is used, the host, and the path to
+# the JS file.
+def parseURI(URI):
+	splitURI = URI.split('/')
+	if len(splitURI) < 4: raise URIError(len(splitURI))
+	if splitURI[0] == 'https:': tls = True
+	elif splitURI[0] == 'http:': tls = False
+	else: raise ProtocolError(splitURI[0])
+	host = splitURI[2]
+	request = '/' + '/'.join(splitURI[3:])
+	filename = splitURI[-1]
+	return tls, host, request, filename
+
+# Perform a TCP traceroute to the host, and return the number of hops required.
+def traceroute(host):
+	traceroute_output = subprocess.check_output(['sudo', 'tcptraceroute',
+						     host])
+	return int(traceroute_output.split(b'\n')[-2].split()[0])
+
+# Construct the HTTP GET request string.
+def gen_message(host, request, referer):
+	message = 'GET /' + request + ' HTTP/1.1\r\n'
+	message += 'Host: ' + host + '\r\n'
+	message += 'Connection: close\r\n'
+	message += 'Cache-Control: max-age=0\r\n'
+	message += 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4)'
+	message += ' AppleWebKit/537.36 (KHTML, like Gecko) '
+	message += 'Chrome/44.0.2403.125 Safari/537.36\r\n'
+	message += 'Referer: ' + referer + '\r\n'
+	message += 'Accept-Encoding: identity\r\n'
+	message += 'Accept-Language: en-US,en;q=0.8\r\n\r\n'
+	return message.encode('utf8')
+
+# Issue the HTTP GET request repeatedly for a given referer and script,
+# recording the amount of time it takes for each
+def issue_request(host, message, tls=False, ttl=None):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	if tls:
+		s = SSL_CONTEXT.wrap_socket(s, server_hostname=host)
+		port = 443
+	else:
+		port = 80
+	try: s.connect((host, port))
+	except: return None, None
+	if ttl: s.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+	s.send(message)
+	full_response = b''
+	time_before = time.time()
+	while True:
+		try:
+			response = s.recv(BUFSIZE)
+			if response: full_response += response
+			else: break
+		except:
+			break
+	download_time = time.time() - time_before
+	s.close()
+	return full_response, download_time
