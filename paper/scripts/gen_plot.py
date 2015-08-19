cfd12319fbdfe0723d7e29273519d883194d1f80
David Tagatac <dtagatac@cs.columbia.edu>
2015-08-19 15:05:59 -0400

    fix typo in gen_fig.py

--- paper/scripts/gen_plot.py
+++ paper/scripts/gen_plot.py
@@ -8,8 +8,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 if len(sys.argv) < 3:
-	print('Usage: gen_figs.py')
-	sys.exit()
+	print('Usage: gen_plot.py <JSON results file> <PNG plot file>')
+	sys.exit(1)
 results_file = sys.argv[1]
 plot_file = sys.argv[2]
 
@@ -17,7 +17,7 @@ with open(results_file) as f: jsondata = json.load(f)
 x = list()
 for entry in jsondata:
 	if entry['downloaded'] and 'traceroutefailed' not in entry:
-		if 'earlyby' not in script:
+		if 'earlyby' not in entry:
 			entry['earlyby'] = entry['traceroute'] - entry['ttlrequired']
 		x.append(entry['earlyby'])
 
