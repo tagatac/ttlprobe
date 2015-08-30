7cc4ad8dae8e296edeff7eb0512ca313b6183b22
David Tagatac <dtagatac@cs.columbia.edu>
2015-08-30 13:00:12 -0400

    sending response packets, but TTLProbe VM is not receiving them

--- routing/router/packethandler.py
+++ routing/router/packethandler.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 from netfilterqueue import NetfilterQueue
-import binascii
+import time, sys, scapy.all as scapy
 
-def print_and_accept(pkt):
-	with open('packet.pcap', 'w') as f: f.write(binascii.hexlify(pkt.get_payload()))
-	print(pkt.get_payload())
-	pkt.accept()
+def drop_and_reply(pkt):
+	print('hit!')
+	pkt.drop()
+	request = scapy.IP(pkt.get_payload())
+	request_tcp = request.payload
+	with open('adpubs.load', 'rb') as f:
+		response_load = f.read()
+	response_load = response_load.replace(b'TIMESTRING',
+		time.strftime('%a, %d %b %Y %H:%M:%S %Z', time.gmtime()))
+	response_tcp = scapy.TCP()/response_load
+	response_tcp.sport = 'http'
+	response_tcp.dport = request_tcp.sport
+	response = scapy.IP()/response_tcp
+	response.src = '117.79.133.119'
+	response.dst = '192.168.29.49'
+	scapy.send(response, iface='eth0')
 
 nfqueue = NetfilterQueue()
-nfqueue.bind(1, print_and_accept)
+nfqueue.bind(1, drop_and_reply)
 try: nfqueue.run()
 except KeyboardInterrupt: print()
